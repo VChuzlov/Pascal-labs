dfb5e005c8462649fec265adb5a4e83aa3a54ae5
VChuzlov <58879294+VChuzlov@users.noreply.github.com>
2021-05-03 16:11:12 +0700

    11 fixed

---
+++ 2021/LB-11/students/2D03/Poselenova/крейзи паскаль/программа3.pas
@@ -0,0 +1,35 @@
+﻿function f(x,y:real) : real;
+begin 
+  result := (2*x)/(exp(y-x**2))
+end;
+ function Eiler(f: function (x,y:real) : real;
+                      x0, xf, y0, h : real):array of real;
+begin 
+  var count := Trunc ((xf-x0)/h)+1;
+  result :=ArrFill(count,0.0);
+  result [0]:=y0;
+  var x:=x0;
+  for var i:=1 to count-1 do
+  begin
+    result[i] := result[i-1] + h * f(x,result[i-1]);
+    x+=h
+  end;
+end;
+ 
+ 
+ function fa(x:real): real;
+begin 
+  result := ln(exp(x**2)+1)
+end;
+
+begin 
+  var y:= Eiler (f,{1}0,2,ln(2),0.2); // x0 = 0
+  y.PrintLines;
+  println;
+  var x := {1}0.0;
+  for var i:=0 to y.High do 
+  begin
+    Println(abs(y[i]-fa(x))/fa(x)*100);
+    x+=0.2 // шаг 0.2
+  end;
+end.                   
