ae97e318827f77a7d2a9147f7d8e002d79ee0217
VChuzlov <58879294+VChuzlov@users.noreply.github.com>
2021-05-16 22:04:24 +0700

    lb-13

---
+++ 2021/LB-13/ex/Eiler2-1.pas
@@ -0,0 +1,79 @@
+﻿function equations(time: real; c, k: array of real): array of real;
+begin
+  result := ArrFill(c.Length, 0.0);
+  result[0] := -k[0] * c[0] - k[1] * c[0] + k[2] * c[2];
+  result[1] := k[0] * c[0];
+  result[2] := k[1] * c[0] - k[2] * c[2]
+end;
+
+
+function Eiler(f: function(time: real; c, k: array of real): array of real;
+               time, c0, k: array of real; h: real): array of array of real;
+begin
+  var count := Trunc((time[^1] - time[0]) / h) + 1;
+  SetLength(result, count);
+  for var i := 0 to result.High do
+    SetLength(result[i], c0.Length);
+  
+  var t := time[0];
+  result[0] := c0;
+  for var i := 1 to count-1 do
+  begin
+    var right_parts := f(t, result[i-1], k);
+    for var j := 0 to result[i].High do
+      result[i][j] := result[i-1][j] + h * right_parts[j];
+    t += h
+  end;
+end;
+
+
+function get_x(c0, c: real): real;
+begin
+  result := (c0 - c) / c0 * 100
+end;
+
+
+function Eiler(f: function(time: real; c, k: array of real): array of real;
+               x:real; c0, k: array of real; h: real): array of array of real;
+begin
+  SetLength(result, 1);
+  SetLength(result[0], c0.Length+1);
+  
+  var x_ := 0.0;
+  var t := 0.0;
+  var i := 1;
+  result[0] := |t| + c0;
+  while x_ < x do
+  begin
+    SetLength(result, result.Length+1);
+    for var l := 0 to result.High do 
+      SetLength(result[l], c0.Length+1);
+    
+    var right_parts := f(t, result[i-1][1:], k);
+    result[i][0] := t;
+    for var j := 1 to result[i-1].High do
+      result[i][j] := result[i-1][j] + h * right_parts[j-1];
+    x_ := get_x(c0[0], result[i][1]);
+    i += 1;
+    t += h
+  end;
+end;
+
+
+begin
+  var time := Arr(0.0, 10.0);
+  var c0 := Arr(0.7, 0.0, 0.0);
+  var k := Arr(0.21, 0.12, 0.18);
+//  var c := Eiler(equations, time, c0, k, 0.1);
+//  
+//  
+//  for var i := 0 to c.High do
+//  begin
+//    for var j := 0 to c[i].High do
+//      write(c[i][j]:8:4);
+//    writeln
+//  end;
+  
+  var y_ := Eiler(equations, 98.0, c0, k, 1);
+  writeln(y_[^1][0]:8:4, y_[^1][1]:8:4, get_x(c0[0], y_[^1][1]):8:2);
+end.
---
+++ 2021/LB-13/ex/RK3.pas
@@ -0,0 +1,71 @@
+﻿function equations(x: real; y: array of real): array of real;
+begin
+  result := ArrFill(y.Length, 0.0);
+  result[0] := 2 * y[0] - 5 * y[1] + 3;
+  result[1] := 5 * y[0] - 6 * y[1] + 1
+end;
+
+
+function RK(f: function(x: real; y: array of real): array of real;
+               x0, xf: real; y0: array of real; h: real): array of array of real;
+  
+  function sum(a: real; arr1, arr2: array of real): array of real;
+  begin
+    result := ArrFill(arr1.Length, 0.0);
+    for var i := 0 to result.High do
+      result[i] += arr1[i] + a * arr2[i]
+  end;
+
+begin
+  var count := Trunc((xf - x0) / h) + 1;
+  SetLength(result, count);
+  for var i := 0 to result.High do
+    SetLength(result[i], y0.Length);
+  
+  var x := x0;
+  var k1, k2, k3, k4: array of real;
+  result[0] := y0;
+  for var i := 1 to count-1 do
+  begin
+    k1 := f(x, result[i-1]);
+    k2 := f(x + h / 2, sum(h / 2, result[i-1], k1));
+    k3 := f(x + h / 2, sum(h / 2, result[i-1], k2));
+    k4 := f(x + h, sum(h, result[i-1], k3));
+    
+    for var j := 0 to result[i].High do
+      result[i][j] := result[i-1][j] + h / 6 * (k1[j] + 2 * k2[j] + 2 * k3[j] + k4[j]);
+    
+    x += h
+  end;
+end;
+
+
+function fa(x: real): array of real;
+begin
+  result := ArrFill(2, 0.0);
+  result[0] := 5 * exp(-2 * x) * cos(3 * x) + 1;
+  result[1] := exp(-2 * x) * (4 * cos(3 * x) + 3 * sin(3 * x)) + 1
+end;
+
+
+begin
+  var y0 := Arr(1.0879, 1.0550);
+  var y := RK(equations, 2, 3, y0, 0.1);
+  
+  for var i := 0 to y.High do
+  begin
+    for var j := 0 to y[i].High do
+      write(y[i][j]:8:4);
+    writeln
+  end;
+  println;
+  var x := 2.0;
+  for var i := 0 to y.High do
+  begin
+    var ya := fa(x);
+    for var j := 0 to y[i].High do
+      write(abs((y[i][j] - ya[j]) / ya[j]) * 100:8:4);
+    writeln;
+    x += 0.1;
+  end;
+end.
