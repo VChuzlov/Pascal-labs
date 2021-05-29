a1e33c26b30af9e481343974fbbe14a545dad08b
VChuzlov <58879294+VChuzlov@users.noreply.github.com>
2021-05-29 23:52:02 +0700

    lb-15

---
+++ 2021/LB-15/ex/1/rect_method.pas
@@ -0,0 +1,21 @@
+﻿function f(x: real): real;
+begin
+  result := exp(1 / x) / x ** 0.5  
+end;
+
+
+function RectMethod(f: function(x:real):real; x: array of real; n: integer): real;
+begin
+  var h := (x[1] - x[0]) / n;
+  result := 0.0;
+  for var i := 0 to n-1 do
+    result += f(x[0] + h * (i + 0.5));
+  result *= h
+end;
+
+
+begin
+  var x := Arr(1.0, 2.0);
+  var int := RectMethod(f, x, 10);
+  Println($'Rect = {int}')
+end.
---
+++ 2021/LB-15/trap_method.pas
@@ -0,0 +1,28 @@
+﻿function f(x: real): real;
+begin
+  result := 1 / (1 + x ** 2)  
+end;
+
+
+function TrapMethod(f: function(x:real):real; 
+                    x: array of real; n: integer): real;
+begin
+  var h := (x[1] - x[0]) / n;
+  result := 0.0;
+  
+  var x_ := new real[n+1];
+  (x_[0], x_[^1]) := (x[0], x[1]);
+  for var i := 1 to x_.High-1 do
+    x_[i] := x_[i-1] + h;
+  
+  for var i := 0 to x_.High-1 do
+    result += (f(x_[i]) + f(x_[i+1])) / 2;
+  result *= h
+end;
+
+
+begin
+  var x := Arr(0.0, 1.0);
+  var int := TrapMethod(f, x, 10);
+  int.Println;
+end.
