992384b52a75be82b5cf4a8d02bc8d91ea1f086d
VChuzlov <58879294+VChuzlov@users.noreply.github.com>
2021-06-08 08:46:45 +0700

    132

---
+++ 2021/debts/4G02/Ябжанов/лабораторная работа 5.1.1.pas
@@ -0,0 +1,22 @@
+﻿begin
+var a:= matrrandom (3, 3, -5, 5);
+var min, j,prod, i, sum: integer;
+sum:=0;
+prod:=1;
+for i := 0 to 2 do
+  for j :=0 to 2 do 
+    begin
+    if a[i,j]>0
+    then
+      sum:=sum+a[i,j];
+      if 
+      a[i,j]<min
+      then 
+        min:=a[i,j];
+      end;
+      prod:=sum*min;
+      a.Println;
+      sum.println;
+      min.println;
+      prod.Println;
+      end.
