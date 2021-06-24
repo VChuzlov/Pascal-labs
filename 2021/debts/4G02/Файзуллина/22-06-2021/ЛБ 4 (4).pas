1ac7a688bc062f3445d2dbf29508db9b8f19577c
VChuzlov <58879294+VChuzlov@users.noreply.github.com>
2021-06-24 14:54:22 +0700

    faizullina

---
+++ 2021/debts/4G02/Файзуллина/22-06-2021/ЛБ 4 (4).pas
@@ -0,0 +1,22 @@
+﻿begin
+ var b:= ArrRandomreal(7, -3, 10);
+ var a,bm, c: real;
+ var i: integer;
+
+ b.println;
+
+ c:=9.5;
+ a:=7.7;
+ bm := 0;
+  if b.max > c then
+   for i := 0 to 6 do
+   b[i]:= b[i]/a
+  else
+  for i := 0 to 6 do
+   if b[i] > 0 then
+    bm := bm + b[i];
+   for i := 0 to 6 do
+   b[i] := b[i] * bm;
+   b.println;
+
+end.
---
+++ 2021/debts/4G02/Файзуллина/22-06-2021/ЛБ 5 (3).pas
@@ -0,0 +1,16 @@
+﻿begin
+  var a := matrrandom(4, 4, -3, 6);
+  a.Println;
+  var s := 0;
+  var q := 0; 
+  for var i := 0 to a.high do
+    for var j := 0 to a.high do
+      if a[i, j] > 0 then
+      begin
+        s += a[ i, j];
+        q += 1;
+      end;
+      s.println;
+      q.println;
+      println (s/q);
+end.
